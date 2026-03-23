b6cf3184c41036d699a85577c0bca25dfb34e0b5
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-03-23 10:58:34 +0530

    change the data list of destinations

--- frontend/src/Data/BookingPackages.jsx
+++ frontend/src/Data/BookingPackages.jsx
@@ -3,12 +3,12 @@ import { assets } from "../assets/assets";
 export const packages = [
   {
     image: assets.destination1,
-    destination: "Taj Mahal / Agra, Uttar Pradesh",
+    destination: "Jodhpur/ Rajasthan",
     duration: "3 Days / 2 Nights",
-    price: "12,999",
-    tripType: ["Heritage", "Romantic", "Culture"],
+    price: "10,999",
+    tripType: ["Heritage", "Architecture", "Culture"],
     description:
-      "Explore the timeless beauty of the Taj Mahal, visit Agra Fort, and enjoy a romantic evening by the Yamuna River.",
+      "Jodhpur, the Blue City and Sun City of Rajasthan, is a historic city in the Thar Desert founded in 1459 by Rao Jodha.",
   },
   {
     image: assets.destination2,
@@ -21,74 +21,20 @@ export const packages = [
   },
   {
     image: assets.destination3,
-    destination: "Goa / Goa",
-    duration: "5 Days / 4 Nights",
-    price: "25,999",
-    tripType: ["Beach", "Party", "Relax"],
+    destination: "Bikaner / Bikaner, Rajasthan",
+    duration: "3 Days / 2 Nights",
+    price: "8,999",
+    tripType: ["Historic", "Culture", "Architecture"],
     description:
-      "Enjoy golden beaches, vibrant nightlife, and beach shacks. Perfect for groups and solo travelers alike!",
+      "Discover the Bikaner City’s majestic forts, royal palaces, and vibrant bazaars filled with art and history.",
   },
   {
     image: assets.destination4,
-    destination: "Kerala Backwaters / Alleppey, Kerala",
-    duration: "6 Days / 5 Nights",
-    price: "20,999",
-    tripType: ["Nature", "Relax", "Scenic"],
+    destination: "Mount Abu / Sirohi, Rajasthan",
+    duration: "3 Days / 2 Nights",
+    price: "10,999",
+    tripType: ["Historic", "Hill station", "Aravalli"],
     description:
-      "Sail through tranquil backwaters on a private houseboat, surrounded by lush coconut groves and calm waters.",
-  },
-  {
-    image: assets.destination5,
-    destination: "Manali / Himachal Pradesh",
-    duration: "5 Days / 4 Nights",
-    price: "13,499",
-    tripType: ["Adventure", "Snow", "Nature"],
-    description:
-      "Enjoy snow-covered peaks, paragliding, and cozy stays with fellow adventurers in this mountain paradise.",
-  },
-  {
-    image: assets.destination6,
-    destination: "Leh-Ladakh / Jammu & Kashmir",
-    duration: "6 Days / 5 Nights",
-    price: "25,999",
-    tripType: ["Adventure", "Scenic", "Road Trip"],
-    description:
-      "Embark on a thrilling journey through high-altitude passes, stunning lakes, and Buddhist monasteries.",
-  },
-  {
-    image: assets.destination7,
-    destination: "Varanasi / Uttar Pradesh",
-    duration: "5 Days / 4 Nights",
-    price: "12,499",
-    tripType: ["Spiritual", "Culture", "Historic"],
-    description:
-      "Experience the divine Ganga Aarti, serene boat rides, and the timeless spirituality of India’s oldest city.",
-  },
-  {
-    image: assets.destination10,
-    destination: "Rishikesh / Uttarakhand",
-    duration: "4 Days / 3 Nights",
-    price: "15,499",
-    tripType: ["Adventure", "Spiritual", "Nature"],
-    description:
-      "Indulge in yoga retreats, river rafting, and peaceful evenings by the sacred Ganges.",
-  },
-  {
-    image: assets.destination8,
-    destination: "Darjeeling / West Bengal",
-    duration: "5 Days / 4 Nights",
-    price: "24,299",
-    tripType: ["Hill Station", "Nature", "Relax"],
-    description:
-      "Sip hot tea amid rolling hills, enjoy toy train rides, and witness stunning sunrise views from Tiger Hill.",
-  },
-  {
-    image: assets.destination9,
-    destination: "Andaman Islands / Andaman & Nicobar Islands",
-    duration: "5 Days / 4 Nights",
-    price: "30,499",
-    tripType: ["Beach", "Scuba Diving", "Nature"],
-    description:
-      "Dive into turquoise waters, explore coral reefs, and relax on the pristine beaches of Havelock Island.",
+      "The only hill station in the desert state of Rajasthan, renowned for its cool climate, lush green surroundings, and the stunning 11th-century Dilwara Jain Temples, known for intricate marble carvings."
   },
 ];
